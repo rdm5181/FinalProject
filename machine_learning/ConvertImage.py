d4256fcae8f8ff832033190abad175384192bcaf
Patrick <ruejo2013@gmail.com>
2022-11-26 16:54:41 -0500

    created subfolders for each subset of the project

---
+++ machine_learning/ConvertImage.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import os, sys
+import cv2
+import numpy as np
+from pathlib import Path
+
+'''
+Converts all images in a directory to '.npy' format.
+Use np.save and np.load to save and load the images.
+Use it for training your neural networks in ML/DL projects. 
+'''
+
+# Path to image directory
+path = os.path.join("C:",os.sep,"Users","Russell","Documents","Fintech-Workspace","Python_Project","FinalProject","Building")
+dirs = os.listdir( path )
+dirs.sort()
+x_train=[]
+
+def load_dataset():
+    # Append images to a list
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item).convert("RGB")
+            im = np.array(im)
+            x_train.append(im)
+
+if __name__ == "__main__":
+    
+    load_dataset()
+    
+    # Convert and save the list of images in '.npy' format
+    imgset=np.array(x_train)
+    np.save("imgb.npy",imgset)
